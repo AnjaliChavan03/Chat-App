01c92b281d8e4bf9ad3f31b7f747be7e3d184c2a
Anjali Chavan <anjali.chavan0301@gmail.com>
2024-11-28 22:57:32 +0530

    final

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-screen grid lg:grid-cols-2">
+    <div className="h-screen flex justify-center items-center">
       {/* Left Side - Form */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
@@ -106,8 +106,8 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-
-         </div>
+    </div>
   );
 };
+
 export default LoginPage;
